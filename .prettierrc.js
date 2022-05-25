f4ff7be1135dffa19ca9693ad14ca8927fee3e40
王龙鑫 <53904502+wlxweb@users.noreply.github.com>
2022-05-25 22:03:50 +0800

    Update .prettierrc.js

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,7 @@ module.exports = {
   singleQuote: true, // 字符串使用单引号
   semi: true, // 每行末尾自动添加分号
   tabWidth: 2, // tab缩进大小,默认为2
-  useTabs: true, // 使用tab缩进，默认false
+  useTabs: false, // 使用tab缩进，默认false
   printWidth: 1000, // 换行长度，默认80
   jsxBracketSameLine: true, // 设置为true时,将多行JSX元素的 > 放在最后一行的末尾，而不是单独放在下一行
   trailingComma: "all", //多行时尽可能打印尾随逗号
